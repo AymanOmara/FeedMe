74e2166c6af3d4ffd46d95da3e8aa7c9ce27f613
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2021-07-15 20:20:08 +0200

    app icon and enhance Random Screen UI

--- FeedMe/Views/RandomScreen/RandomViewController.swift
+++ FeedMe/Views/RandomScreen/RandomViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 import SDWebImage
 import RxCocoa
 import RxSwift
+import SkeletonView
 class RandomViewController: UIViewController {
-let randomViewModel  = RandomViewModel()
+    
+    
+    let randomViewModel  = RandomViewModel()
 let disposeBag = DisposeBag()
     var secondsRemaining = 5
     @IBOutlet weak var btn: UIButton!
@@ -19,12 +22,35 @@ let disposeBag = DisposeBag()
     @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        btn.layer.borderWidth = 2
+        btn.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+       // btn.layer.cornerRadius = 40
+        btn.layer.cornerRadius = btn.frame.size.width / 2
+        btn.clipsToBounds = true
+//        btn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: btn.bounds.width - btn.bounds.height)
+//        btn.imageView?.layer.cornerRadius = btn.bounds.height/2.0
+        image.alpha = 1
+        image.isSkeletonable = true
+        image.animationDuration = 60
+        image.showGradientSkeleton()
+        
+        image.startAnimating()
+        image.backgroundColor = .belizeHole
+        
         self.navigationController?.setNavigationBarHidden(true, animated: true)
-        randomViewModel.featchData()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+            self.randomViewModel.featchData()
+            
+        }
         randomViewModel.dataObservable.subscribe({(data) in
-            self.image.sd_setImage(with: URL(string: data.element!.strMealThumb), placeholderImage: UIImage(named: "placeholder"))
-            self.mealName.text = data.element?.strMeal
-            self.mealCategory.text = data.element?.strCategory
+
+            self.image.sd_setImage(with: URL(string: data.element!.strMealThumb), completed:{_,_,_,_ in
+                self.image.stopSkeletonAnimation()
+                self.image.hideSkeleton()
+            })
+            self.mealName.text = data.element!.strMeal
+            self.mealCategory.text = "Category:" + data.element!.strCategory
         }).disposed(by: disposeBag)
         self.name()
 
@@ -39,9 +65,9 @@ let disposeBag = DisposeBag()
                 print ("\(self.secondsRemaining) seconds")
                 self.secondsRemaining -= 1
             } else if self.secondsRemaining == 0{
-             //   self.btn.setTitle("")
-                self.btn.setTitle("", for: .normal)
-                self.btn.setImage(UIImage(named: "close-icon"), for: UIControl.State.normal)
+             
+                self.btn.setTitle("X", for: .normal)
+             //   self.btn.setImage(UIImage(named: "close-icon"), for: UIControl.State.normal)
                 self.btn.addTarget(self, action: #selector(self.btnPressed), for: UIControl.Event.touchUpInside)
             }
         }
@@ -51,14 +77,7 @@ let disposeBag = DisposeBag()
         let navigationController = self.storyboard?.instantiateViewController(withIdentifier: "NavigationController") as! NavigationController
         navigationController.modalPresentationStyle = .fullScreen
         self.present(navigationController, animated: true, completion: nil)
-        //self.navigationController?.pushViewController(navigationController, animated: true)
-     //   viewcontroller.modalPresentationStyle = .fullScreen
-      //  self.navigationController?.pushViewController(viewcontroller, animated: true)
-//        let viewcontroller = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
-//        self.tabBarController?.navigationController?.pushViewController(viewcontroller, animated: true)
-       // self.tabBarController?.navigationController?.pushViewController(viewcontroller, animated: true)
-       //self.tabBarController?.show(navigationController, sender: self)
-//        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(viewcontroller)
+
     }
 
 }
